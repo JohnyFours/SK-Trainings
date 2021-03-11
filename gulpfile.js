c91f038b2ecef08d1c42f20cd9f12c18921e0a0f
Aleksandr <aleks.protasov@outlook.com>
2021-03-11 21:29:48 +0200

    added training12

--- gulpfile.js
+++ gulpfile.js
@@ -64,7 +64,8 @@ function browsersync() {
     exports.browsersync = browsersync;
     // exports.cleandist = cleandist;
     
-    exports.build = series(cleandist, cleanimg, styles, images ,buildcopy);
+    // exports.build = series(cleandist, cleanimg, styles, images ,buildcopy);
+    exports.build = series(cleandist, styles, buildcopy);
     exports.default = parallel(browsersync, startWatch);
 
 
